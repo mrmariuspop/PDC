1d84748d11e4b1dc27ae1d0ca0b58935ac564f72
DOM_MEDIASAT\popma <popma@mediamarktsaturn.com>
2018-10-22 16:14:28 +0300

    lucrarea 8

---
+++ PDC/lucrarea8/aprogram.c
@@ -0,0 +1,81 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdlib.h"
+
+int is_prime(unsigned long n)
+{
+	for (unsigned long i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	unsigned long* numbers;
+	unsigned long lStart=0, lEnd=0, failure, mult, len;
+	
+	scanf("%d", &lStart);
+	scanf("%d", &lEnd);
+
+
+	/*if (argc != 2)
+	{
+		lStart = 1;
+		lEnd = 100000;
+	}
+	else
+	{
+		lStart = atol(argv[1]);
+		lEnd = atol(argv[2]);
+	}*/
+
+	if (lStart < 2) lStart = 2;
+	if (lStart > lEnd) return 0;
+
+	len = lEnd - lStart + 1;
+	numbers = new unsigned long[len];
+
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+	{
+		numbers[i] = i + lStart;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (is_prime(numbers[i]) == 0)
+		{
+			numbers[i] = 0;
+			mult = (i + lStart) * (i + lStart);
+			while (mult - lStart <= len)
+			{
+				numbers[mult - lStart] = 0;
+				mult += (i + lStart);
+			}
+		}
+	}
+
+	printf("Result:\n");
+	for (i = 0; i < len; i++)
+	{
+		if (numbers[i] > 0)
+		{
+			printf("%d\n", i + lStart);
+		}
+	}
+	
+	
+	delete[] numbers;
+	failure = 0;
+	system("pause");
+	return failure;
+
+}
